4e69f456e22983ce7ceb534be2746bc789fe5863
4kpros <prosper.abouar@gmail.com>
2022-11-03 22:49:11 +0100

    JAudioTagger 2.2.6 added

---
+++ app/src/main/java/com/prosabdev/fluidmusic/models/SongItem.kt
@@ -0,0 +1,33 @@
+package com.prosabdev.fluidmusic.models
+
+import org.jaudiotagger.tag.images.Artwork
+
+class SongModel {
+    var id = 0
+    var fileName: String? = null
+    var title: String? = null
+    var artist: String? = null
+    var albumArtist: String? = null
+    var album: String? = null
+    var genre: String? = null
+    var relativePath: String? = null
+    var absolutePath: String? = null
+    var folder: String? = null
+    var year: String? = null
+    var duration: Long = 0
+
+    //
+    var typeMime: String? = null
+    var sampleRate = 0
+    var bitrate = 0.0
+
+    //
+    var lastUpdateDate: String? = null
+    var lastAddedDate: String? = null
+
+    //
+    var storageName: String? = null
+
+    //
+    var covertArt: Artwork? = null
+}
---
+++ app/src/main/java/com/prosabdev/fluidmusic/utils/AudioFileInfoExtractor.kt
@@ -0,0 +1,176 @@
+package com.prosabdev.fluidmusic.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.prosabdev.fluidmusic.R
+import com.prosabdev.fluidmusic.models.SongItem
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.audio.exceptions.CannotReadException
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException
+import org.jaudiotagger.tag.FieldKey
+import org.jaudiotagger.tag.Tag
+import org.jaudiotagger.tag.TagException
+import org.jaudiotagger.tag.images.Artwork
+import java.io.File
+import java.io.IOException
+
+
+class AudioFileInfo {
+
+    fun getAudioInfo(absolutePath: String?, songItem: SongItem) {
+        if (absolutePath != null && absolutePath.isNotEmpty()) {
+            val tempFile = File(absolutePath)
+            try {
+                val audioFile = AudioFileIO.read(tempFile)!!
+
+                // First get audio header tagger
+                val audioHeader = audioFile.audioHeader
+                val format = audioHeader.format
+                val bitrate = audioHeader.bitRateAsNumber.toDouble()
+                val sampleRate = audioHeader.sampleRateAsNumber / 1000
+
+                // After get audio content tagger
+                val tag: Tag = audioFile.tagOrCreateAndSetDefault!!
+
+                //Retrieve covert art
+                val artwork: Artwork = tag.firstArtwork
+                val title: String = tag.getFirst(FieldKey.TITLE)
+                val artist: String = tag.getFirst(FieldKey.ARTIST)
+                val album: String = tag.getFirst(FieldKey.ALBUM)
+                val genre: String = tag.getFirst(FieldKey.GENRE)
+                val year: String = tag.getFirst(FieldKey.YEAR)
+                val comment: String = tag.getFirst(FieldKey.COMMENT)
+                val lyrics: String = tag.getFirst(FieldKey.LYRICS)
+                songItem.covertArt = artwork
+                songItem.title = title
+                songItem.artist = artist
+                songItem.album = album
+                songItem.genre = genre
+                songItem.year = year
+                songItem.sampleRate = sampleRate
+                songItem.typeMime = format
+                songItem.bitrate = bitrate
+            } catch (e: CannotReadException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: TagException) {
+                e.printStackTrace()
+            } catch (e: ReadOnlyFileException) {
+                e.printStackTrace()
+            } catch (e: InvalidAudioFrameException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun getBitmapAudioArtwork(context: Context, path: String?, width: Int, height: Int): Bitmap? {
+        var tempBitmapImage: Bitmap? = null
+        if (path != null && path.isNotEmpty()) {
+            var binaryDataImage: ByteArray? = ByteArray(0)
+            try {
+                binaryDataImage = getAudioBinaryDataArtwork(path)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            try {
+                if (binaryDataImage != null && binaryDataImage.isNotEmpty()) {
+                    val options: BitmapFactory.Options = BitmapFactory.Options()
+                    options.inJustDecodeBounds = true
+                    BitmapFactory.decodeByteArray(binaryDataImage, 0, binaryDataImage.size, options)
+
+                    // Calculate inSampleSize
+                    options.inSampleSize = calculateInSampleSize(options, width, height)
+
+                    // Decode bitmap with inSampleSize set
+                    options.inJustDecodeBounds = false
+                    try {
+                        tempBitmapImage = BitmapFactory.decodeByteArray(
+                            binaryDataImage,
+                            0,
+                            binaryDataImage.size,
+                            options
+                        )
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                } else {
+                    tempBitmapImage = BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.ic_fluid_music_icon
+                    )
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return tempBitmapImage
+    }
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        // Raw height and width of image
+        val height: Int = options.outHeight
+        val width: Int = options.outWidth
+        var inSampleSize = 1
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight = height / 2
+            val halfWidth = width / 2
+            while (halfHeight / inSampleSize >= reqHeight
+                && halfWidth / inSampleSize >= reqWidth
+            ) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
+    }
+
+    private fun getAudioBinaryDataArtwork(absolutePath: String?): ByteArray? {
+        var binaryDataArtwork = ByteArray(0)
+        if (absolutePath != null && absolutePath.isNotEmpty()) {
+            val tempFile = File(absolutePath)
+            try {
+                val audioFile = AudioFileIO.read(tempFile)!!
+                // After get audio content tagger
+                val tag: Tag = audioFile.tagOrCreateAndSetDefault!!
+                binaryDataArtwork = tag.firstArtwork.binaryData
+            } catch (e: CannotReadException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: TagException) {
+                e.printStackTrace()
+            } catch (e: ReadOnlyFileException) {
+                e.printStackTrace()
+            } catch (e: InvalidAudioFrameException) {
+                e.printStackTrace()
+            }
+        }
+        return binaryDataArtwork
+    }
+
+    fun getAudioArtwork(absolutePath: String?): Artwork? {
+        var artwork: Artwork? = null
+        if (absolutePath != null && absolutePath.isNotEmpty()) {
+            val tempFile = File(absolutePath)
+            try {
+                val audioFile = AudioFileIO.read(tempFile)!!
+                // After get audio content tagger
+                val tag: Tag = audioFile.tagOrCreateAndSetDefault!!
+                artwork = tag.firstArtwork
+            } catch (e: CannotReadException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: TagException) {
+                e.printStackTrace()
+            } catch (e: ReadOnlyFileException) {
+                e.printStackTrace()
+            } catch (e: InvalidAudioFrameException) {
+                e.printStackTrace()
+            }
+        }
+        return artwork
+    }
+}
